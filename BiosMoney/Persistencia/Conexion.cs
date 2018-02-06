61347a542b96424a285201e81e4bf98db0c1c6da
rafiki11 <dilangames11@hotmail.com>
2018-02-06 00:39:22 -0300

    .

---
+++ BiosMoney/Persistencia/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Persistencia.Clases
+{
+   internal class Conexion
+    {
+       public const String STR = "Data Source=.; Initial Catalog=BiosMoney;Integrated Security=True";
+
+    }
+}
