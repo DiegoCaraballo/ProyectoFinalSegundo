5f71036b74b31b0cb4b3650dc15be9bf1263a042
rafiki11 <dilangames11@hotmail.com>
2018-02-14 22:12:52 -0300

    se sube por 120372183 vez

---
+++ EntidadesCompartidas/Pago.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+namespace EntidadesCompartidas
+{
+    [DataContract]
+   public class Pago
+    {
+    }
+}
