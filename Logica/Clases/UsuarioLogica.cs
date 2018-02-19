80a64f4c3f8672275556705f60264e1714780929
rafiki11 <dilangames11@hotmail.com>
2018-02-19 20:29:40 -0300

    agregue referencia a persistencia en logica. y clases de fabrica, subo para evitar errores si las crean ustedes.

---
+++ Logica/Clases/UsuarioLogica.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using EntidadesCompartidas;
+
+namespace Logica
+{
+   internal class UsuarioLogica : IUusuarioLogica
+    {
+
+    }
+}
