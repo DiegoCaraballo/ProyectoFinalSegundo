58c009fdef31b4eaf05784fcf07b1783b7c2a6e4
DiegoCaraballo <diegocaraballo84@gmail.com>
2018-02-20 19:02:24 -0300

    Agrego persitencia de pago y metodo buscar

---
+++ Persistencia/Clases/PagoPersistencia.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using EntidadesCompartidas;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Persistencia.Clases
+{
+    internal class PagoPersistencia : IPagoPersistencia
+    {
+        #region SINGLETON
+
+        private static PagoPersistencia instancia = null;
+        private PagoPersistencia() { }
+        public static PagoPersistencia GetInstancia()
+        {
+            if (instancia == null)
+                instancia = new PagoPersistencia();
+            return instancia;
+        }
+
+        #endregion
+
+        #region Operaciones
+
+        public void AltaPago(Pago unPago)
+        {
+ 
+        }
+
+        public void BajaPago(Pago unPago)
+        {
+
+        }
+
+        public void ModificarPago(Pago unPago)
+        {
+
+        }
+
+        public Pago BuscarPago(int numeroInt)
+        {
+            SqlConnection cnn = new SqlConnection(Conexion.Cnn);
+
+            Pago unPago = null;
+
+            SqlCommand cmd = new SqlCommand("BuscarPago", cnn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@numeroInt", numeroInt);
+
+            try
+            {
+                cnn.Open();
+                SqlDataReader lector = cmd.ExecuteReader();
+                if (lector.HasRows)
+                {
+                    int _numeroInt = (int)lector["numeroInt"];
+                    DateTime _fecha = (DateTime)lector["fecha"];
+                    int _montoTotal = (int)lector["montoTotal"];
+                    int _cedulaCajero = (int)lector["cedulaCajero"];
+                    unPago = new Pago(_numeroInt, _fecha, _montoTotal, CajeroPersistencia.GetInstancia().BuscarCajero(_cedulaCajero));
+                }
+                lector.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                cnn.Close();
+            }
+            return unPago;
+        }
+        #endregion
+    }
+}
---
+++ Persistencia/Interfaces/IPagoPersistencia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using EntidadesCompartidas;
+
+namespace Persistencia
+{
+    public interface IPagoPersistencia
+    {
+        void AltaPago(Pago unPago);
+        void BajaPago(Pago unPago);
+        void ModificarPago(Pago unPago);
+        Pago BuscarPago(int numeroInt);
+    }
+}
